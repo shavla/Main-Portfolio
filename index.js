0b32108ce64a99bf673ead036fc931e0e752b960
shavla <rakviashvili95@gmail.com>
2020-11-07 13:34:02 +0400

    new

---
+++ index.js
@@ -0,0 +1,25 @@
+function funcHome() {
+  $(".home").css("display", "inline");
+  $(".about").css("display", "none");
+  $(".work").css("display", "none");
+  $(".contact").css("display", "none");
+}
+function funcAbout() {
+  $(".home").css("display", "none");
+  $(".about").css("display", "inline");
+  $(".work").css("display", "none");
+  $(".contact").css("display", "none");
+}
+
+function funcWork() {
+  $(".home").css("display", "none");
+  $(".about").css("display", "none");
+  $(".work").css("display", "inline");
+  $(".contact").css("display", "none");
+}
+function funcContact() {
+  $(".home").css("display", "none");
+  $(".about").css("display", "none");
+  $(".work").css("display", "none");
+  $(".contact").css("display", "inline");
+}
